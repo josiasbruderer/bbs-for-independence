e8c1b00ecb3e6d7679d2a8c6ab068860d1de0273
Josias Bruderer <josias@macherstube.ch>
2021-08-28 21:50:20 +0200

    prepare and implement metadata-filtering

--- 03_workspace/main.py
+++ 03_workspace/main.py
@@ -39,83 +39,118 @@ The module data_wrangler will be used for this.
 """
 
 number_of_threads = 24
+skip_steps = ["download", "cleaning"]
 data_url = "http://archives.textfiles.com/[name].zip"
-data_names = ["100", "adventure", "anarchy", "apple", "art", "artifacts", "bbs", "computers", "conspiracy", "digest",
-              "drugs", "etext", "exhibits", "floppies", "food", "fun", "games", "groups",
-              "hacking", "hamradio", "history", "holiday", "humor", "internet", "law", "magazines", "media", "messages",
-              "music", "news", "occult", "phreak", "piracy", "politics", "programming", "reports", "rpg", "science",
-              "sex", "sf", "stories", "survival", "tap", "ufo", "uploads", "virus"]  # the categories to download
-data_names_exclude = ["fidonet-on-the-internet"] # categories that are excluded and removed from data_names
+data_names = ["100"]
+"""
+    , "adventure", "anarchy", "apple", "art", "artifacts", "bbs", "computers", "conspiracy", "digest",
+              "drugs", "etext", "exhibits", "floppies", "food", "fun", "games", "groups", "hacking", "hamradio",
+              "history", "holiday", "humor", "internet", "law", "magazines", "media", "messages", "music", "news",
+              "occult", "phreak", "piracy", "politics", "programming", "reports", "rpg", "science", "sex", "sf",
+              "stories", "survival", "tap", "ufo", "uploads", "virus", "fidonet-on-the-internet"]  # categories to download
+"""
+data_names_exclude = ["fidonet-on-the-internet", "tap", "floppies", "exhibits", "artifacts",
+                      "piracy", "art"] # categories that are excluded and removed from data_names
 file_filter = "^.*(\.(jpe?g|png|gif|bmp|zip|mp3|wav))|index\.html?$" # use this to exlude by filenames
+metadata_file_filter = "x['metadata']['charratioB'] > 0.95" #
 data_dir = Path(project_path / "02_datasets/")
 tmp_dir = Path(project_path / ".tmp/")
 
 threads = []
 
-# prepare the threads for loading data
-for names in helpers.chunker_list(data_names, number_of_threads):
-    threads.append(wrangler.loader(tmp_dir, data_dir, data_url, names))
+if "download" not in skip_steps:
+    # prepare the threads for loading data
+    for names in helpers.chunker_list(data_names, number_of_threads):
+        threads.append(wrangler.loader(tmp_dir, data_dir, data_url, names))
 
-# start all threads
-for thread in threads:
-    thread.start()
-
-# wait for all threads to finish their work
-running = True
-while running:
-    running = False
+    # start all threads
     for thread in threads:
-        if thread.is_alive():
-            running = True
-            print("waiting for threads to finish...")
-            time.sleep(1)
+        thread.start()
 
-# prepare the threads for cleaning up stuff
-threads = []
-dataset = {}
+    # wait for all threads to finish their work
+    running = True
+    while running:
+        running = False
+        for thread in threads:
+            if thread.is_alive():
+                running = True
+                print("waiting for threads to finish...")
+                time.sleep(1)
 
-# prepare the threads
-for names in helpers.chunker_list(data_names, number_of_threads):
-    threads.append(wrangler.cleaner(data_dir, names, file_filter))
+    print("data downloaded successfully")
 
-# start all threads
-for thread in threads:
-    thread.start()
+if "cleaning" not in skip_steps:
+    # prepare the threads for cleaning up stuff
+    threads = []
+    dataset = {}
 
-# wait for all threads to finish their work
-running = True
-while running:
-    running = False
+    # prepare the threads
+    for names in helpers.chunker_list(data_names, number_of_threads):
+        threads.append(wrangler.cleaner(data_dir, names, file_filter))
+
+    # start all threads
     for thread in threads:
-        if thread.is_alive():
-            running = True
-            print("waiting for threads to finish...")
-            time.sleep(1)
-        else:
-            if thread.data:
-                dataset.update(thread.data)
-                for d in thread.data:
-                    helpers.save_object(thread.data[d], tmp_dir.joinpath(str(d + ".pkl")))
+        thread.start()
 
-helpers.save_object(dataset, tmp_dir.joinpath("dataset.pkl"))
+    # wait for all threads to finish their work
+    running = True
+    while running:
+        running = False
+        for thread in threads:
+            if thread.is_alive():
+                running = True
+                print("waiting for threads to finish...")
+                time.sleep(1)
+            else:
+                if thread.data:
+                    dataset.update(thread.data)
+                    for d in thread.data:
+                        helpers.save_object(thread.data[d], tmp_dir.joinpath(str(d + ".pkl")))
 
-# write metadata to csv file
-print("Write dataset to csv")
-f = open(tmp_dir.joinpath("dataset.csv"), "w+")
-f.write("category,name,path,length,length_raw,avgcolumnsize,charratioA,charratioB,year,eyear,lyear,type\r\n")
-for d in dataset:
-    for item in dataset[d]:
-        f.write("\"" + d + "\",\"" + str(item["metadata"]["name"])+"\",\""+
-                str(item["metadata"]["path"])+"\","+
-                str(item["metadata"]["length"])+","+
-                str(item["metadata"]["length_raw"])+","+
-                str(item["metadata"]["avgcolumnsize"])+","+
-                str(item["metadata"]["charratioA"])+","+
-                str(item["metadata"]["charratioB"])+",\""+
-                str(item["metadata"]["year"])+"\","+
-                str(item["metadata"]["eyear"])+","+
-                str(item["metadata"]["lyear"])+",\""+
-                str(item["metadata"]["type"])+"\"\r\n")
-f.close()
+    helpers.save_object(dataset, tmp_dir.joinpath("dataset_full.pkl"))
 
-print("data downloaded successfully")
+    # write metadata to csv file
+    print("Write dataset to csv")
+    f = open(tmp_dir.joinpath("dataset.csv"), "w+")
+    f.write("category,name,path,length,length_raw,avgcolumnsize,charratioA,charratioB,year,eyear,lyear,type\r\n")
+    for d in dataset:
+        for item in dataset[d]:
+            f.write("\"" + d + "\",\"" + str(item["metadata"]["name"])+"\",\""+
+                    str(item["metadata"]["path"])+"\","+
+                    str(item["metadata"]["length"])+","+
+                    str(item["metadata"]["length_raw"])+","+
+                    str(item["metadata"]["avgcolumnsize"])+","+
+                    str(item["metadata"]["charratioA"])+","+
+                    str(item["metadata"]["charratioB"])+",\""+
+                    str(item["metadata"]["year"])+"\","+
+                    str(item["metadata"]["eyear"])+","+
+                    str(item["metadata"]["lyear"])+",\""+
+                    str(item["metadata"]["type"])+"\"\r\n")
+    f.close()
+
+    print("data cleaned successfully")
+else:
+    # load dataset.pkl because it was not generate during runtime
+    dataset = helpers.load_object(tmp_dir.joinpath("dataset_full.pkl"))
+    print("data loaded from dataset_full.pkl")
+
+if "netadata-filtering" not in skip_steps:
+    for key in data_names_exclude:
+        if key in dataset:
+            del dataset[key]
+
+    for key in dataset:
+        d_tmp = []
+        for x in dataset[key]:
+            if eval(metadata_file_filter):
+                d_tmp.append(x)
+        dataset[key] = d_tmp
+
+    helpers.save_object(dataset, tmp_dir.joinpath("dataset_filtered.pkl"))
+else:
+    # load dataset.pkl because it was not generate during runtime
+    dataset = helpers.load_object(tmp_dir.joinpath("dataset_filtered.pkl"))
+    print("data loaded from dataset_filtered.pkl")
+
+if "modeling" not in skip_steps:
+    print("lets get modeling started")
