509781c050635cabb4605e479e5d0fc09fc362b5
Josias Bruderer <josias@macherstube.ch>
2021-08-30 08:31:55 +0200

    cleanup states to fix git-lfs

--- 03_workspace/states/state_080_300_30000_declaration/main_snapshot.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb31fd4d3f641e853337fdfb5b7419a6abdc6817c570bef768bb505d714f6e46
-size 21157
--- 03_workspace/states/state_095_300_30000_categories/main_snapshot.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b121778ca8264c4608493263c3fb7a5b4ab5b17454ef0c79bb455a6d83e2d60
-size 21690
